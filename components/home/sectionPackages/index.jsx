edd26d16321f08040639943de609ab1b217d84c3
Serkan <serkanuckc123@gmail.com>
2024-12-08 20:56:10 +0300

    text fix

--- components/home/sectionPackages/index.jsx
+++ components/home/sectionPackages/index.jsx
@@ -36,21 +36,24 @@ const SectionPackages = () => {
     <div className="bg-packagesBackground">
       <PageContainer className="flex items-center justify-center flex-col">
         <div className=" flex flex-col items-center font-Inter justify-center mx-[120px] mb-[32px] mt-[40px]">
-          <div className="section-header text-primary">
+          <div className="section-header font-lexend text-nowrap text-[#915DFF] text-base md:text-2xl">
             İşletmenizi Büyütmek İçin
           </div>
-          <div className="section-header text-secondary">
+          <div className="section-header font-lexend font-bold  text-nowrap text-[#2F1566] text-base md:text-2xl">
             En Doğru Veri Paketini Seçin
           </div>
         </div>
         <div className="flex items-center justify-center  flex-col">
           <p
             style={{ color: "rgba(66, 82, 107, 1)" }}
-            className="text-[18px] mb-[32px]  leading-[24px] font-Inter "
+            className="text-[18px]  text-center leading-[24px] font-Inter mb-[32px] 
+             whitespace-normal sm:whitespace-pre-line sm:leading-[24px] sm:text-left"
           >
             Her sektöre, her ihtiyaca ve her bütçeye uygun veri çözümleriyle, iş
+            {"\n"}
             hedeflerinizi kolayca gerçekleştirin.
           </p>
+
           <div className="w-[318px] mx-[px] my-[6px] h-[52px] px-[12px] rounded-lg bg-[#ffffff]  shadow-sectionPackages py-[12px]">
             <span className="mr-[12px]  text-[#838E9E] text-[16px] font-lexend font-semibold leading-[24px] ">
               Paketler
