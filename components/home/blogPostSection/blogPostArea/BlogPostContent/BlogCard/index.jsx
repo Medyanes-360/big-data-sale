9b78aadb3cb32242ecac19509c9d03de39a085ae
Öykü Kaya <oykuky001@gmail.com>
2024-12-08 23:54:23 +0300

    blogcard style modified

--- components/home/blogPostSection/blogPostArea/BlogPostContent/BlogCard/index.jsx
+++ components/home/blogPostSection/blogPostArea/BlogPostContent/BlogCard/index.jsx
@@ -27,16 +27,15 @@ const BlogCard = ({ title, summary, author, likes, comments, date }) => {
         </div>
       </div>
       <div className="mt-6 mb-3">  
-        {/* kontol ett mt-2 mb-4 dene c */}
         <h2 className="text-[#061C3D] font-extrabold text-[16px] md:text-[20px] leading-4 md:leading-6 font-Inter">
           {title}
         </h2>
       </div>
-      <p className="text-[14px] md:text-[18px] leading-4 md:leading-6 text-[#42526B] font-normal mb-[24px]">
+      <p className="text-[14px] md:text-[18px] leading-4 md:leading-6 text-[#42526B] font-normal mb-6">
         {summary}
       </p>
       <div className="flex flex-col md:flex-row items-center justify-between gap-1 md:gap-2">
-        <ButtonComponent className="w-full md:max-w-[190px] flex items-center gap-[12px] !px-4 bg-tertiary-500 rounded-[7px]">
+        <ButtonComponent className="w-full md:max-w-[190px] flex items-center gap-3 !px-4 bg-tertiary-500 rounded-[7px]">
           <span className="text-[#FFFFFF] text-[13.9px] font-Inter font-medium">
             Daha Fazla
           </span>
