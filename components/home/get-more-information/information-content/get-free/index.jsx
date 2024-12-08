db78643dde712b3f2a86c93cb2b4aae73fb54dbc
Serkan <serkanuckc123@gmail.com>
2024-12-08 19:19:35 +0300

    informationframe update responsive and  badge update

--- components/home/get-more-information/information-content/get-free/index.jsx
+++ components/home/get-more-information/information-content/get-free/index.jsx
@@ -4,12 +4,12 @@ import ButtonComponent from "@/globalElements/Button";
 import Icons from "@/public/assets/icons/Icons";
 const GetStartFree = () => {
   return (
-    <ButtonComponent className="flex items-center flex-wrap justify-center gap-x-3 mx-auto sm:max-w-[427px] lg:mx-0 w-full lg:w-fit bg-lavenderGray p-2 mb-0 lg:mb-10 rounded-[15.57px]">
+    <ButtonComponent className="flex !items-center  flex-wrap !justify-between md:p-4 md:w-[424px] md:h-[73px] bg-lavenderGray gap-x-3 mx-auto sm:max-w-[427px] lg:mx-0 w-full lg:w-fit bg-lavenderGray  mb-0 lg:mb-10 rounded-[15.57px]">
       <Badge title="Yeni🎉" />
       <span className="text-fiord-700 text-[12px] leading-4 sm:text-lg font-Inter font-medium">
         Ücretsiz Denemeye Başla
       </span>
-      <Icons.ArrowRight className="w-4 h-4 md:w-8 md:h-8" color="#2F1566" />
+      <Icons.ArrowRight className="w-8 h-8 md:w-8 md:h-8" color="#2F1566" />
     </ButtonComponent>
   );
 };
--- components/home/get-more-information/information-content/information-frame/index.jsx
+++ components/home/get-more-information/information-content/information-frame/index.jsx
@@ -4,21 +4,25 @@ import React from "react";
 const InformationFrame = () => {
   return (
     <div className="flex flex-col gap-y-3">
-      <h5 className="text-2xl font-Inter text-fiord-700">
-        Doktorlar, Avukatlar, Yerel İşletmeler, Fabrikalar ve Daha Binlerce
-        Sektör Datasına Erişin!
+      <h5 className="text-base max-w-[343px] text-[#42526B] font-bold md:text-2xl lg:mb-6	 lg:text-xl md:font-normal md:text-[24px] font-Inter md:text-fiord-700 md:whitespace-nowrap whitespace-normal">
+        Doktorlar, Avukatlar, Yerel İşletmeler,
+        <span className="block">
+          {" "}
+          Fabrikalar ve Daha Binlerce Sektör Datasına Erişin!
+        </span>
       </h5>
-      <h1 className="font-Inter font-bold lg:text-[31.15px] lg:leading-[51.91px] text-tertiary-800">
-        <span className="text-tertiary400">
+      <h1 className="text-base text-[#42526B] lg:font-Inter  lg:text-nowrap	 lg:text-[31.15px] lg:leading-[51.91px] md:text-tertiary-800  font-bold">
+        <span className="text-tertiary400 text-base lg:font-Inter lg:text-nowrap	 lg:text-[31.15px]">
           Doğrulanmış İşletme Bilgileriyle Binlerce
         </span>{" "}
+        <br />
         Potansiyel Müşteri Verilerine Erişin!
       </h1>
       <div className="flex items-center gap-4 flex-col md:flex-row">
-        <ButtonComponent className="text-tertiary-800 bg-gray-24 shadow-custom-combined  bg-white-default  w-full h-[52px] flex items-center justify-center lg:max-w-[257px] rounded-[9.08px] font-Inter font-medium text-[18.17px] leading-[51.91px]">
+        <ButtonComponent className="text-tertiary-800 bg-gray-24 shadow-custom-combined bg-white-default w-full h-[52px] flex items-center justify-center lg:max-w-[257px] rounded-[9.08px] font-Inter font-medium text-[18.17px] leading-[51.91px]">
           Daha Fazla Bilgi Al
         </ButtonComponent>
-        <ButtonComponent className="bg-tertiary-800 h-[52px] flex drop-shadow-custom-combined-2   w-full items-center justify-center lg:max-w-[257px] rounded-[9.08px] font-Inter font-medium text-[18.17px] leading-[51.91px] text-white-default">
+        <ButtonComponent className="bg-tertiary-800 h-[52px] flex drop-shadow-custom-combined-2 w-full items-center justify-center lg:max-w-[257px] rounded-[9.08px] font-Inter font-medium text-[18.17px] leading-[51.91px] text-white-default">
           Hemen Başla
         </ButtonComponent>
       </div>
