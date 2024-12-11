760273200ec0121088c103302a3327eacdc7003d
Serkan <serkanuckc123@gmail.com>
2024-12-11 23:51:38 +0300

    input responsive and web fix

--- components/home/get-more-information/information-image/advertisingBox/index.jsx
+++ components/home/get-more-information/information-image/advertisingBox/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AdvertisingBox = ({ text, icon, className }) => {
   return (
     <div
-      className={`drop-shadow-custom-combined backdrop-blur-custom w-fit h-[36.7px] sm:h-[56.14px] flex items-center justify-center gap-x-2 lg:gap-x-5 px-3 lg:px-5 bg-white-50 border border-solid border-silver-400 rounded-[16px] ${className}`}
+      className={`drop-shadow-custom-combined !px-3 backdrop-blur-custom w-fit h-[36.7px] sm:h-[56.14px] flex items-center justify-center gap-x-2 lg:gap-x-5 px-3 lg:px-5 bg-white-50 border border-solid border-silver-400 rounded-[16px] ${className}`}
     >
       <span className="text-black text-[9px] leading-4 md:text-sm font-Inter">
         {text}
--- components/home/get-more-information/information-image/emailBox/index.jsx
+++ components/home/get-more-information/information-image/emailBox/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const EmailBox = () => {
   return (
-    <div className="shadow-custom mb-4  backdrop-blur-custom absolute right-0  md:left-[-30] md:left-[-50%] lg:left-auto -top-[35px] w-fit min-h-[36.69px] sm:min-h-[56.14px] flex items-center justify-center gap-x-4 px-3 bg-white-50 border border-solid border-silver-400 rounded-[10px] sm:rounded-[16px]">
+    <div className="shadow-custom mb-4  backdrop-blur-custom absolute right-0  md:left-[-30] md:left-[-50%] lg:left-auto -top-[35px] w-fit min-h-[36.69px] sm:min-h-[56.14px] flex items-center justify-center gap-x-4 md:px-3 bg-white-50 border border-solid border-silver-400 rounded-[10px] sm:rounded-[16px]">
       <input
         type="email"
         className="text-black text-sm font-Inter border-none outline-none h-full p-3"
