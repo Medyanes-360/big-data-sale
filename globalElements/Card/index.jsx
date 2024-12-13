557359105792ab2447aa011b3a004f721b5f6bb0
Serkan <98692987+Serkan-Ucakcioglu@users.noreply.github.com>
2024-12-13 11:31:33 +0300

    leading fix

--- globalElements/Card/index.jsx
+++ globalElements/Card/index.jsx
@@ -49,7 +49,7 @@ const Card = ({
     </div>
 
     <div className="mx-[11.43px] mb-[11px] flex items-center space-x-3">
-      <span className="font-bold text-tertiary-800 text-[30.48px] leading-[51px]">
+      <span className="font-bold text-tertiary-800 text-[30.48px] leading-[51.43px]">
         {price}
       </span>
       <span className="font-Inter font-medium text-gray-700 text-[13.3px] leading-[20px]">
