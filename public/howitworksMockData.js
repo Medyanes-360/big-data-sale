c5ad3c1e1681a7e22ac74706d73ce9d2cc594518
Öykü Kaya <oykuky001@gmail.com>
2024-12-17 13:51:40 +0300

    fix: container padding and added mock data file

---
+++ public/howitworksMockData.js
@@ -0,0 +1,108 @@
+const howitworksMockData = [
+    {
+      order: "01.",
+      title: "İhtiyacınıza uygun paketi seçin",
+      description:
+        "Küçük başlangıçlardan büyük projelere kadar tüm ihtiyaçlarınıza uygun paket seçenekleri sunuyoruz. Sadece hedeflediğiniz ülke ve sektörleri belirleyin, gerisini bize bırakın!",
+      footerTitle: "Planlı Yatırım, Büyük Kazanç",
+      footerDescription:
+        "İster girişimci, ister profesyonel olun, doğru veri paketleriyle pazarlama gücünüzü artırabilirsiniz. Her paket, stratejik hedeflerinizi büyütmek için özel olarak tasarlanmıştır. ",
+      listTitle: "Onaylanmış Maddeler",
+      dataList: [
+        {
+          description: "Küçükten büyüğe geniş paket seçenekleri",
+        },
+        {
+          description: "Hedefe özel veri sunumu",
+        },
+        {
+          description: "Kolay ve hızlı satın alma",
+        },
+        {
+          description: "Stratejinizi destekleyen veriler",
+        },
+      ],
+      color: "#E6DBFF",
+      shadow: "0px 10px 6px 0px #4B3E6747",
+    },
+    {
+      order: "02.",
+      title: "İhtiyacınıza uygun paketi seçin",
+      description:
+        "Küçük başlangıçlardan büyük projelere kadar tüm ihtiyaçlarınıza uygun paket seçenekleri sunuyoruz. Sadece hedeflediğiniz ülke ve sektörleri belirleyin, gerisini bize bırakın!",
+      footerTitle: "Planlı Yatırım, Büyük Kazanç",
+      footerDescription:
+        "İster girişimci, ister profesyonel olun, doğru veri paketleriyle pazarlama gücünüzü artırabilirsiniz. Her paket, stratejik hedeflerinizi büyütmek için özel olarak tasarlanmıştır. ",
+      listTitle: "Onaylanmış Maddeler",
+      dataList: [
+        {
+          description: "Küçükten büyüğe geniş paket seçenekleri",
+        },
+        {
+          description: "Hedefe özel veri sunumu",
+        },
+        {
+          description: "Kolay ve hızlı satın alma",
+        },
+        {
+          description: "Stratejinizi destekleyen veriler",
+        },
+      ],
+      color: "#915DFF",
+      shadow: "0px 10px 6px 0px #E6DBFF30",
+    },
+    {
+      order: "03.",
+      title: "İhtiyacınıza uygun paketi seçin",
+      description:
+        "Küçük başlangıçlardan büyük projelere kadar tüm ihtiyaçlarınıza uygun paket seçenekleri sunuyoruz. Sadece hedeflediğiniz ülke ve sektörleri belirleyin, gerisini bize bırakın!",
+      footerTitle: "Planlı Yatırım, Büyük Kazanç",
+      footerDescription:
+        "İster girişimci, ister profesyonel olun, doğru veri paketleriyle pazarlama gücünüzü artırabilirsiniz. Her paket, stratejik hedeflerinizi büyütmek için özel olarak tasarlanmıştır. ",
+      listTitle: "Onaylanmış Maddeler",
+      dataList: [
+        {
+          description: "Küçükten büyüğe geniş paket seçenekleri",
+        },
+        {
+          description: "Hedefe özel veri sunumu",
+        },
+        {
+          description: "Kolay ve hızlı satın alma",
+        },
+        {
+          description: "Stratejinizi destekleyen veriler",
+        },
+      ],
+      color: "#6437C5",
+      shadow: "0px 10px 6px 0px #E6DBFF30",
+    },
+    {
+      order: "04.",
+      title: "İhtiyacınıza uygun paketi seçin",
+      description:
+        "Küçük başlangıçlardan büyük projelere kadar tüm ihtiyaçlarınıza uygun paket seçenekleri sunuyoruz. Sadece hedeflediğiniz ülke ve sektörleri belirleyin, gerisini bize bırakın!",
+      footerTitle: "Planlı Yatırım, Büyük Kazanç",
+      footerDescription:
+        "İster girişimci, ister profesyonel olun, doğru veri paketleriyle pazarlama gücünüzü artırabilirsiniz. Her paket, stratejik hedeflerinizi büyütmek için özel olarak tasarlanmıştır. ",
+      listTitle: "Onaylanmış Maddeler",
+      dataList: [
+        {
+          description: "Küçükten büyüğe geniş paket seçenekleri",
+        },
+        {
+          description: "Hedefe özel veri sunumu",
+        },
+        {
+          description: "Kolay ve hızlı satın alma",
+        },
+        {
+          description: "Stratejinizi destekleyen veriler",
+        },
+      ],
+      color: "#451E99",
+      shadow: "0px 10px 6px 0px #E6DBFF30",
+    },
+  ];
+
+  export default howitworksMockData
