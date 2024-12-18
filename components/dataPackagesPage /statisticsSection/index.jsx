3ce9db9211aca243f210314e3b533c1ebb6d4d15
Serkan <serkanuckc123@gmail.com>
2024-12-18 21:28:53 +0300

    data created and mass icon add

--- components/dataPackagesPage /statisticsSection/index.jsx
+++ components/dataPackagesPage /statisticsSection/index.jsx
@@ -1,6 +1,13 @@
 import Icons from "@/public/assets/icons/Icons";
 import React from "react";
 
+const staticSectionData = [
+  { title: "2024", mass: "Year founded" },
+  { title: "300", mass: "Employees" },
+  { title: "1000K", mass: "Users" },
+  { title: "220M", mass: "Data entries" },
+];
+
 function StaticsSection() {
   return (
     <section className="w-full bg-white flex flex-col items-center py-6 gap-6 px-[16px] lg:px-[120px]  lg:h-[405px] ">
@@ -10,6 +17,8 @@ function StaticsSection() {
       <div className="text-center font-Inter font-bold text-[32px] leading-[54px]">
         Temel <span className="text-tertiary400">değerlerimiz</span>
       </div>
+
+      <div></div>
     </section>
   );
 }
