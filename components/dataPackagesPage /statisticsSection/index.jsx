a67bcaaeabe1552e606eb6657728852ea0addc48
Serkan <serkanuckc123@gmail.com>
2024-12-18 20:51:23 +0300

    Data Packages Page - Statistics section create folder and file - add containers

---
+++ components/dataPackagesPage /statisticsSection/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function StaticsSection() {
+  return (
+    <section className="w-full h-[405px] bg-white flex flex-col gap-6 px-[120px] py-6">
+      test
+    </section>
+  );
+}
+
+export default StaticsSection;
--- containers/dataPackages/index.jsx
+++ containers/dataPackages/index.jsx
@@ -1,9 +1,11 @@
 import SectionPackages from "@/components/home/sectionPackages";
+import DataPackagesPages from "../../components/dataPackagesPage /index";
 
 export default function DataPackagesPageContainer() {
   return (
     <>
       <SectionPackages />
+      <DataPackagesPages />
     </>
   );
 }
