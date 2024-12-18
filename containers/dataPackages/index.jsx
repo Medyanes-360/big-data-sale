cdfe6e295f6d66a87c2b9a627bf0ae81db26e111
Serkan <serkanuckc123@gmail.com>
2024-12-18 22:10:41 +0300

    cursor-pointer add and  datapackgesContainer add faq component

--- containers/dataPackages/index.jsx
+++ containers/dataPackages/index.jsx
@@ -1,11 +1,13 @@
 import SectionPackages from "@/components/home/sectionPackages";
 import DataPackagesPages from "../../components/dataPackagesPage /index";
+import Faq from "@/components/home/questinAnswer";
 
 export default function DataPackagesPageContainer() {
   return (
     <>
       <SectionPackages />
       <DataPackagesPages />
+      <Faq />
     </>
   );
 }
