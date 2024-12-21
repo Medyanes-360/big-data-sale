4a2864ac04251d4b07e237ac8c03804849970044
Serkan <98692987+Serkan-Ucakcioglu@users.noreply.github.com>
2024-12-21 22:32:07 +0300

    indent fix

--- containers/blog/index.jsx
+++ containers/blog/index.jsx
@@ -1,7 +1,6 @@
 import BlogImportantIssues from "@/components/blog/blogImportantIssues";
 import BlogSection from "@/components/blog/blogSection";
 
-
 export default function BlogPageContainer() {
   return (
     <>
--- containers/successStory/index.jsx
+++ containers/successStory/index.jsx
@@ -2,12 +2,10 @@ import Testimonials from "@/components/home/testimonials";
 import SuccessStories from "@/components/successStories";
 
 export default function SuccessStoryPageContainer() {
-
-
   return (
     <div className="min-h-screen flex flex-col items-center">
-        <SuccessStories />
-        <Testimonials />    
+      <SuccessStories />
+      <Testimonials />
     </div>
   );
 }
