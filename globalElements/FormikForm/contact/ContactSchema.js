204d6784abf901709fb3212e3c6696ebbd700ca6
Serkan <98692987+Serkan-Ucakcioglu@users.noreply.github.com>
2024-12-22 20:11:13 +0300

    ContactSchhema required add description

--- globalElements/FormikForm/contact/ContactSchema.js
+++ globalElements/FormikForm/contact/ContactSchema.js
@@ -5,22 +5,22 @@ export let contactSchema = object({
     .min(5, "Minimum 5 harften oluşur.")
     .max(20, "Maximum 20 karakter!")
     .matches(/^[a-zA-Z]+$/, "Sadece harf !")
-    .required(),
+    .required("Name zorunlu"),
   email: string()
     .email()
     .matches(
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       "Lütfen doğru bir Email Adresi Giriniz"
     )
-    .required(),
+    .required("Email Zorunlu"),
   company: string()
     .matches(/^[a-zA-Z]+$/, "Sadece harf !")
     .min(5, "Minimum 5 Karakter")
     .max(30, "Maximum 30 Karakter")
-    .required(),
+    .required("Company Name zorunlu"),
   message: string()
     .min(10, "Minimum 10 karakter")
     .max(50, "Maximum 50 karakter")
-    .required(),
+    .required("Message zorunlu"),
   createdOn: date().default(() => new Date()),
 });
