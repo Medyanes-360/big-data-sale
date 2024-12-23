18833ac4ad378e5feb0df9a30e7d8d25851bf81c
YildizGokhan <gygokhanyildiz1@gmail.com>
2024-12-23 21:29:23 +0300

    some bugs fixed

--- components/home/get-more-information/information-content/get-free/badge/index.jsx
+++ components/home/get-more-information/information-content/get-free/badge/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Badge = ({ title }) => {
   return (
-    <span className="flex w-fit p-3 items-center  font-Inter font-medium bg-tertiary-800 text-[12px] sm:text-lg rounded-[10.38px] h-[41.53px] text-white-default">
+    <span className="flex  w-1/4 p-3 justify-center items-center  font-Inter font-medium bg-tertiary-800 text-[12px] sm:text-[18px] rounded-[10.38px] h-[41.53px] text-white-default">
       {title}
     </span>
   );
