3517f5d59411e4405f6461bf3bc03629bc1ea478
Serkan <98692987+Serkan-Ucakcioglu@users.noreply.github.com>
2024-12-27 22:20:53 +0300

    mock data created

--- components/contactPage/supportSection/index.jsx
+++ components/contactPage/supportSection/index.jsx
@@ -1,6 +1,14 @@
 "use client";
 import React from "react";
 
+const support = [
+  {
+    title: "Support",
+    btntitle: "Contact Support",
+  },
+  { title: "PartnerShip", btntitle: "Email Adress" },
+];
+
 function supportSection() {
   return <div></div>;
 }
