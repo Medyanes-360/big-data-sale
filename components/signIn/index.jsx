f3c8621e95cfd986d162c9d9cd2768a2ca07c19e
Serkan <serkanuckc123@gmail.com>
2024-12-31 20:24:05 +0300

    signın create

---
+++ components/signIn/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function signInPage() {
+  return <>test</>;
+}
+
+export default signInPage;
---
+++ containers/signInPage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignInPage from "../../components/signIn/index";
+
+function signInPageContainer() {
+  return (
+    <>
+      <SignInPage />
+    </>
+  );
+}
+
+export default signInPageContainer;
