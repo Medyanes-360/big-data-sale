dfb67718a7625c702de5fb623be606ee025619df
Serkan <98692987+Serkan-Ucakcioglu@users.noreply.github.com>
2025-01-01 17:05:52 +0300

    update card

--- components/signInPage/chooseUs/index.jsx
+++ components/signInPage/chooseUs/index.jsx
@@ -17,20 +17,52 @@ function ChooseUs() {
       </h3>
 
       <div className="card bg-tertiary-900 w-[384px] signCard h-[475px] rounded-2xl	p-6 gap-2">
-        <div className="flex border border-gray600 p-3 rounded-xl gap-3">
+        <div className="flex items-center border border-gray600 p-3 rounded-xl gap-3">
           <Image
             src={Avatar}
             width={48}
             height={48}
             alt="Picture of the author"
           />
-          <div className="flex flex-col">
-            <span className="text-white-default">Amanda</span>
-            <span className="text-white-default">Amanda Test</span>
+          <div className="flex flex-col gap-1">
+            <span className="text-white-default font-lexend font-light text-sm text-light200">
+              Amanda L.
+            </span>
+            <span className="font-Inter font-normal text-[10px] leading-[15px] text-tertiary-50">
+              Yerel Restoranlarla Çalışan Bir Grafik Tasarımcı
+            </span>
           </div>
         </div>
-        <div></div>
-        <div></div>
+        <div className="flex flex-col justify-center h-[147px] p-3 gap-3 border-b border-gray600 ">
+          <span className="font-lexend font-normal text-xs text-light200">
+            <strong className="font-lexend font-normal text-xs text-tertiary400">
+              Sektör:
+            </strong>{" "}
+            Grafik Tasarım
+          </span>
+          <span className="font-lexend font-normal text-xs text-light200">
+            <strong className="font-lexend font-normal text-xs text-tertiary400">
+              Hedef:
+            </strong>{" "}
+            Restoran menüleri ve marka kimliği için tasarım projeleri yapmak.
+          </span>
+          <span className="font-lexend font-normal text-xs text-light200">
+            <strong className="font-lexend font-normal text-xs text-tertiary400">
+              Çözüm:
+            </strong>{" "}
+            Eksik görsel kimlikleri olan restoranları tespit ederek onlara
+            ulaşmaya başladı.
+          </span>
+        </div>
+        <div className="mt-2">
+          <p className="font-Inter font-normal text-base text-white-default">
+            "Benim işim görsel tasarımla restoranların marka değerini artırmak.
+            Sunduğunuz verilerle web sitesi olmayan veya dijital görünürlüğü
+            zayıf restoranlarla çalışmaya başladım. 7 ayda 35 farklı restoran
+            için projeler yaptım. Bu işletmelere ihtiyacını net bir şekilde
+            gösterince hemen ilgilendiler." — Amanda L.
+          </p>
+        </div>
       </div>
     </div>
   );
--- components/signInPage/index.jsx
+++ components/signInPage/index.jsx
@@ -4,7 +4,7 @@ import ChooseUs from "./chooseUs";
 
 function SignInPage() {
   return (
-    <div className="flex justify-between p-[24px] pt-[24px] pl-[24px] pb-[20px] pr-[48px]">
+    <div className="flex justify-between p-[24px] pt-[24px] pl-[24px] pb-[20px] pr-[48px] bg-black">
       <ChooseUs />
 
       <div>login</div>
