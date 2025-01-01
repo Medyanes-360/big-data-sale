060094cd7901bfb54996219122b6963da03ff9b2
Serkan <98692987+Serkan-Ucakcioglu@users.noreply.github.com>
2025-01-01 23:10:01 +0300

    loginForm component create and add

--- components/signInPage/index.jsx
+++ components/signInPage/index.jsx
@@ -1,13 +1,14 @@
 "use client";
 import React from "react";
 import ChooseUs from "./chooseUs";
+import LoginForm from "./loginForm";
 
 function SignInPage() {
   return (
-    <div className="flex justify-between h-screen p-[24px] pt-[24px] pl-[24px] pb-[20px] pr-[48px]">
+    <div className="flex justify-between items-center h-screen p-[24px] pt-[24px] pl-[24px] pb-[20px] pr-[48px]">
       <ChooseUs />
 
-      <div>login</div>
+      <LoginForm />
     </div>
   );
 }
---
+++ components/signInPage/loginForm/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoginForm() {
+  return (
+    <div className="flex flex-1 items-center justify-center h-full">
+      Login Form
+    </div>
+  );
+}
+
+export default LoginForm;
