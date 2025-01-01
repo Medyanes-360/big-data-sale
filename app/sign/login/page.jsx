3cdf7ac258583a5e9a7a6082e07b11f75629957a
Serkan <98692987+Serkan-Ucakcioglu@users.noreply.github.com>
2025-01-01 15:29:17 +0300

    created sign

---
+++ app/sign/login/page.jsx
@@ -0,0 +1,12 @@
+import SignInPageContainer from "@/containers/signInPage";
+import React from "react";
+
+function page() {
+  return (
+    <>
+      <SignInPageContainer />
+    </>
+  );
+}
+
+export default page;
