90c280e4002e8783ce1c22cfc5d40b9ba5298998
Serkan <98692987+Serkan-Ucakcioglu@users.noreply.github.com>
2025-01-03 23:14:50 +0300

    update

--- components/signInPage/chooseUs/index.jsx
+++ components/signInPage/chooseUs/index.jsx
@@ -8,7 +8,7 @@ function ChooseUs() {
   return (
     <>
       <div className="max-w-[480px] h-full flex flex-col-reverse lg:flex-row gap-8  items-center ">
-        <div className="w-full flex-[1]  bg-bg-gradient h-full flex flex-col items-center justify-center rounded-xl md:gap-6 py-12">
+        <div className="w-[383px] md:w-[480px] flex-[1]  bg-bg-gradient h-full flex flex-col items-center justify-center rounded-xl md:gap-6 py-12">
           <Image
             src={wellnes}
             alt="Welness"
