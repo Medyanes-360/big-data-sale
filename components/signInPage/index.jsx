03aaf49259728e39cb378fc51f56b00b9ebc5e15
Serkan <98692987+Serkan-Ucakcioglu@users.noreply.github.com>
2025-01-03 16:31:59 +0300

    update form responsive logo add

--- components/signInPage/index.jsx
+++ components/signInPage/index.jsx
@@ -3,10 +3,19 @@ import React from "react";
 import ChooseUs from "./chooseUs";
 import LoginForm from "./loginForm";
 import Icons from "@/public/assets/icons/Icons";
+import wellnes from "@/public/assets/logo/welness_black.webp";
+import Image from "next/image";
 
 function SignInPage() {
   return (
     <div className="flex flex-col-reverse items-center gap-[61px] pt-11 px-4  lg:flex-row lg:justify-between  h-screen lg:p-[24px] lg:pt-[24px] lg:pl-[24px] lg:pb-[20px] lg:pr-[48px]">
+      <Image
+        src={wellnes}
+        alt="Welness"
+        width={110}
+        className="block md:!hidden "
+        height={43.9}
+      />
       <ChooseUs />
       <LoginForm />
       <div className="fixed z-50 right-10 bottom-10 flex justify-center items-center rounded-full bg-tertiary-800 w-20 h-20 cursor-pointer">
