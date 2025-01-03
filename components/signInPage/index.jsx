ca8de86a2b2666bf453f1e66aebb573d39a1d9c0
Serkan <98692987+Serkan-Ucakcioglu@users.noreply.github.com>
2025-01-04 00:01:09 +0300

    update

--- components/signInPage/index.jsx
+++ components/signInPage/index.jsx
@@ -8,20 +8,22 @@ import Image from "next/image";
 
 function SignInPage() {
   return (
-    <div className="flex flex-col-reverse h-full items-center gap-[61px] pt-11 md:px-4  lg:flex-row lg:justify-between  lg:h-screen lg:p-[24px] lg:pt-[24px] lg:pl-[24px] lg:pb-[20px] lg:pr-[48px]">
-      <ChooseUs />
-      <LoginForm />
-      <Image
-        src={wellnes}
-        alt="Welness"
-        width={110}
-        className="block md:!hidden"
-        height={43.9}
-      />
-      <div className="fixed z-50 transform -translate-y-1/2 right-8 bottom-8 flex justify-center items-center rounded-full bg-tertiary-800 w-20 h-20 cursor-pointer">
-        <Icons.MailIcon />
+    <>
+      <div className="flex flex-col-reverse h-full items-center gap-[61px] pt-11 lg:px-4  lg:flex-row lg:justify-between  lg:h-screen lg:p-[24px] lg:pt-[24px] lg:pl-[24px] lg:pb-[20px] lg:pr-[48px]">
+        <ChooseUs />
+        <LoginForm />
+        <Image
+          src={wellnes}
+          alt="Welness"
+          width={110}
+          className="block md:!hidden"
+          height={43.9}
+        />
+        <div className="fixed z-50 transform -translate-y-1/2 right-8 bottom-8 flex justify-center items-center rounded-full bg-tertiary-800 w-20 h-20 cursor-pointer">
+          <Icons.MailIcon />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
